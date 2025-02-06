53041453eb62ad0c0a7c550b820e84cea4db468a
Abotteen <adel.abotteen@gmail.com>
2025-02-06 10:49:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+VonageDialer.init({}, (dialer) => {
+  dialer.setOnDialerEvent((event) => {
+    const logDiv = document.createElement("div");
+    logDiv.innerHTML = '<b>Event:</b> ' + JSON.stringify(event);
+    logDiv.style.paddingBottom = '10px';
+    document.body.appendChild(logDiv);
+  });
 
+  // dialer.setVisibility(true);
+});
